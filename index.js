765839b7b23b617918d8cf533d566cd9d5dabe61
chris lott <peanutlott96@gmail.com>
2022-03-19 11:27:58 -0400

    commit tree 370662d601a94d650752ce8c94d6c313804821b4

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(f){
+  if (f <= 400) {
+    return `This one is on me!`
+  }
+  if (f > 2500) {
+    return `No can do.`
+  }
+  if (f > 2000) {
+    return `I will gladly take your thirty bucks.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(c){
+  if (c === `NYC`) {
+    return `Ok, sounds good.`
+  } else if (c !== `NYC`) {
+    return `No go.`
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  if (t === `generous`) {
+    return `Thank you so much.`
+  } else if (t === `not as generous`) {
+    return `Thank you.`
+  } else {
+    return `Bye.`
+  }
 }
